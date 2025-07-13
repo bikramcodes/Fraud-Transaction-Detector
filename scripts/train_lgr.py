ad82e235c4d2374ac2113774a5aa3a6983306916
Bikram Singh <bikram.hanspal99@gmail.com>
2025-07-14 00:16:58 +0530

    Project completed

--- scripts/train_lgr.py
+++ scripts/train_lgr.py
@@ -2,6 +2,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+import joblib
+from pathlib import Path
 
 
 
@@ -25,9 +27,6 @@ def report(y_true, y_cap):
     print()
     print(accuracy_score(y_true, y_cap))
     print()
-
-import joblib
-from pathlib import Path
 
 def save_model(model, path, name):
     Path(path[:-1]).mkdir(parents=True, exist_ok=True)
